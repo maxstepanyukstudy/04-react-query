bc9c14ceeb65ff5aa84cc512b3d0f63393ec62da
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-23 10:14:02 +0300

    npm i axios, modern-normalize with declarations.d.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import 'modern-normalize'
 import './index.css'
+
 import App from './components/App/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
