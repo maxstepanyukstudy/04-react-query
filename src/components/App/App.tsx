186aea081fbdb36a50fd46997ba1a7d6ea348d90
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-23 22:40:49 +0300

    App use SearchBar and MovieGrid

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import toast, { Toaster } from "react-hot-toast";
+
 export default function App() {
-  return <></>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  function handleSeqrch(query: string) {
+    const handleFetchMovies = async () => {
+      try {
+
+        setIsLoading(true);
+        setIsError(false);
+        const data = await fetchMovies(query);
+        if (data.length < 1) {
+          toast("No movies found for your request.");
+        }
+        setMovies(data);
+
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    handleFetchMovies();
+  }
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSeqrch} />
+
+      <main>
+        <MovieGrid movies={movies} />
+      </main>
+
+      <Toaster />
+    </>
+  );
 }
